9230cd143810febdcbf3ca690e4ce7dd4cb46834
席庆辉 <1010991002@qq.com>
2020-07-06 17:58:51 +0800

    分支第一次提交

--- git/src/git/Test.java
+++ git/src/git/Test.java
@@ -9,4 +9,14 @@ public class Test {
     public int hashCode() {
         return super.hashCode();
     }
+
+    @Override
+    public boolean equals(Object obj) {
+        return super.equals(obj);
+    }
+
+    @Override
+    public String toString() {
+        return super.toString();
+    }
 }
