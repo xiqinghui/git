7b8d237358a267520af32a11e68cffeb680b8842
席庆辉 <1010991002@qq.com>
2020-07-06 17:55:22 +0800

    sss第一次修改

--- git/src/git/Test.java
+++ git/src/git/Test.java
@@ -4,4 +4,9 @@ public class Test {
     public static void main(String[] args) {
         System.out.println("ssssss");
     }
+
+    @Override
+    public int hashCode() {
+        return super.hashCode();
+    }
 }
